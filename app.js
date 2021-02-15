e4c63183f1886391152dd7d980cf33b6f7441afb
Brown Mamba <brdyherrera@gmail.com>
2021-02-15 18:36:45 -0500

    added styles sheet and final functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
 const inquirer = require("inquirer");
 const generatePage = require("./src/page-template");
+const { writeFile, copyFile } = require("./utils/generate-site");
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -138,22 +138,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw new Error(err);
-
-      console.log(
-        "Page created! Check out index.html in this directory to see it!"
-      );
-    });
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
   });
-
-// const pageHTML = generatePage(name, github);
-
-//The first argument is the file name that will be created, or the output file. The second argument is the data that's being written: the HTML string template. The third argument is the callback function that will handle any errors as well as the success message.
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
