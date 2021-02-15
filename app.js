732ef62f8e17df1a2157235b23b785a70e117492
Brown Mamba <brdyherrera@gmail.com>
2021-02-14 19:13:30 -0500

    downloaded inquirer package, and generate the first question

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
 //The first argument is the file name that will be created, or the output file. The second argument is the data that's being written: the HTML string template. The third argument is the callback function that will handle any errors as well as the success message.
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
