221a743312d85c88c70f1771e75821e4a666c0fc
Brown Mamba <brdyherrera@gmail.com>
2021-02-15 16:51:16 -0500

    added projects and formatted the layout of the html page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generatePage = require("./src/page-template.js");
+const generatePage = require("./src/page-template");
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -48,10 +48,10 @@ const promptUser = () => {
 
 const promptProject = (portfolioData) => {
   console.log(`
-  =================
-  Add a New Project
-  =================
-  `);
+=================
+Add a New Project
+=================
+`);
 
   // If there's no 'projects' array property, create one
   if (!portfolioData.projects) {
@@ -138,13 +138,15 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+
+      console.log(
+        "Page created! Check out index.html in this directory to see it!"
+      );
+    });
   });
 
 // const pageHTML = generatePage(name, github);
